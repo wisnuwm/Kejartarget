efc28483f93333e791f67be32800477d5d0c3208
wisnuwm <54229493+wisnuwm@users.noreply.github.com>
2019-08-25 19:23:12 +0700

    yy

---
+++ soaltuple.py
@@ -0,0 +1,22 @@
+#buat list
+list1 = [123,54,12,56,23,54,20]
+#remove 54
+list1.remove(54)
+
+# list to tuple
+tuple1 = tuple (list1)
+print (tuple1)
+
+print(list1)
+(123,54,12,56,23,54,20)
+
+type(list1)
+
+type(tuple1)
+
+#panjang tuple
+print(len(tuple1))
+
+#tampilkan nilai 
+x = tuple1.index(56)
+print(x)
---
+++ tuple.py
@@ -0,0 +1,7 @@
+tuple1 = ()
+tuple1 = tuple()
+tuple1 = ('bondo')
+tuple2 = ('bondo','nekat')
+
+print (tuple1)
+print (tuple1[1])
